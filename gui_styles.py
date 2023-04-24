8530aec9dfe3b263d0941d382bb0499fd9351e75
jreeb <jreebel@gmail.com>
2023-04-24 15:15:40 -0400

    Added gui_styles.py

---
+++ gui_styles.py
@@ -0,0 +1,15 @@
+import arcade.gui
+
+RED_STYLE = {
+            "font_name": ("calibri", "arial"),
+            "font_size": 15,
+            "font_color": arcade.color.WHITE,
+            "border_width": 2,
+            "border_color": None,
+            "bg_color": arcade.color.RED,
+
+            # used if button is pressed
+            "bg_color_pressed": arcade.color.WHITE,
+            "border_color_pressed": arcade.color.PINK,  # also used when hovered
+            "font_color_pressed": arcade.color.RED,
+            }
