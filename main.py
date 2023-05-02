5b4b571d8c4218373e11a1f565b080effdc498a0
jreeb <jreebel@gmail.com>
2023-05-02 11:55:49 -0400

    Added gui_styles.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 import arcade
-# import arcade.gui
 from gui_styles import *
 
 # --- Constants ---
@@ -8,7 +7,8 @@ SPRITE_SCALING_PLAYER = 0.5
 
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 900
-SCREEN_TITLE = "Mathman II"
+SCREEN_TITLE = "Mathman 2023"
+PLAYER_MOVEMENT_SPEED = 1
 
 
 # class QuitButton(arcade.gui.UIFlatButton):
@@ -98,6 +98,12 @@ class GameView(arcade.View):
         self.player_sprite = None
         self.score = 0
 
+        # Track the current state of what key is pressed
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+
         arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
@@ -116,44 +122,83 @@ class GameView(arcade.View):
         # Character image from kenney.nl
         self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png",
                                            SPRITE_SCALING_PLAYER)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
+        self.player_sprite.center_x = SCREEN_WIDTH / 2
+        self.player_sprite.center_y = SCREEN_HEIGHT / 2
         self.player_list.append(self.player_sprite)
 
     def on_draw(self):
         """ Draw everything """
         self.clear()
         self.player_list.draw()
-
+        print(f'player x = {self.player_sprite.center_x} and y = {self.player_sprite.center_y}')
         # Put the text on the screen.
         output = f"Score: {self.score}"
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
 
-    def on_mouse_motion(self, x, y, dx, dy):
-        """ Handle Mouse Motion """
-
-        # Move the center of the player sprite to match the mouse x, y
-        self.player_sprite.center_x = x
-        self.player_sprite.center_y = y
-
     def on_update(self, delta_time):
         """ Movement and game logic """
-        pass
+        self.player_sprite.center_x += self.player_sprite.change_x
+        self.player_sprite.center_y += self.player_sprite.change_y
 
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
+        # Call update on all sprites
+        self.player_list.update()
+
         # Generate a list of all sprites that collided with the player.
-        # coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
 
         # Loop through each colliding sprite, remove it, and add to the score.
-        # for coin in coins_hit_list:
-        #     coin.remove_from_sprite_lists()
-        #     self.score += 1
 
-        # If the coin list is empty, we are done, go to game over view
-        # if len(self.coin_list) == 0:
+        # If the problem list is empty, we are done, go to game over view
+        # if len(self.problem_list) == 0:
         #     view = GameOverView()
         #     self.window.show_view(view)
+
+    def process_keychange(self):
+        """
+        Called when we change a key up/down
+        """
+        # Process up/down
+        if self.up_pressed and not self.down_pressed:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif self.down_pressed and not self.up_pressed:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        else:
+            self.player_sprite.change_y = 0
+
+        # Process left/right
+        if self.right_pressed and not self.left_pressed:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+        elif self.left_pressed and not self.right_pressed:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        else:
+            self.player_sprite.change_x = 0
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed."""
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.up_pressed = True
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.down_pressed = True
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.left_pressed = True
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.right_pressed = True
+
+        self.process_keychange()
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.up_pressed = False
+            self.jump_needs_reset = False
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.down_pressed = False
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.left_pressed = False
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.right_pressed = False
+
+        self.process_keychange()
 
 
 def main():
